271d28bc56a324fb55b7a24ebdd5e4930cb354f3
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-07-17 23:08:26 +0600

    login form customized

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+// ADMIN ROUTES
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function() {
+
+    Route::get('/home', 'AdminController@index')->name('admin.home');
+
+
+    // Authentication Routes...
+    Route::get('/login', 'Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\LoginController@login');
+    Route::post('/logout', 'Auth\LoginController@logout')->name('admin.logout');
+
+
+});
